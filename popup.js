bfb25d0925a4cb5e2d0553f81cc1e3f56e2787a2
Yash Shrestha <yrshrestha@growbydata.com>
2024-06-02 18:02:39 +0545

    fix: update api for bulk insert

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 const FRONTEND_URL = "http://100.25.194.112:3000";
-const BACKEND_URL = "http://100.25.194.112:4449";
+const BACKEND_URL = "http://127.0.0.1:4449";
 
 document.getElementById('scrape').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -7,7 +7,7 @@ document.getElementById('scrape').addEventListener('click', () => {
         target: { tabId: tabs[0].id },
         function: scrapePrice
       }, (results) => {
-
+        
         if(results[0].result.price === 'Price not found'){
           document.getElementById('price').textContent = 'Please try to visit a single product page!'
           return true;
@@ -19,21 +19,52 @@ document.getElementById('scrape').addEventListener('click', () => {
   });
   
   function scrapePrice() {
-    const priceElement = document.querySelector('.pdp-price.pdp-price_type_normal.pdp-price_color_orange.pdp-price_size_xl');
-    const titleElement = document.title;
-    const descriptionElement = document.querySelector('.html-content.pdp-product-highlights');
-    const imageElement = document.querySelector('.gallery-preview-panel__image');
-    const urlElement = window.location.href;
-    const categoryElement = document.querySelectorAll('.breadcrumb_item_anchor')[2];
-    return {
-      'price': priceElement ? priceElement.innerText : 'Price not found',
-      'imageElement': imageElement ? imageElement.src : 'Image not found',
-      'descriptionElement': descriptionElement ? descriptionElement.innerHTML : 'Description not found',
-      'titleElement': titleElement ? titleElement : 'Title not found',
-      'urlElement': urlElement ? urlElement : 'URL not found',
-      'categoryElement': categoryElement ? categoryElement.innerText : 'Category not found'
-    };
-    // return priceElement ? priceElement.innerText : 'Price not found';
+    const categoryType = window.location.pathname.split('/')[1];
+    if(categoryType !== 'products'){
+      
+      const productList = [];
+      const productElements = document.querySelectorAll('.gridItem--Yd0sa');
+
+      productElements.forEach(product => {
+        
+
+        const price = product.querySelector('.current-price--Jklkc')?.innerText;
+        const title = product.querySelector('.title-wrapper--IaQ0m')?.innerText;
+        const description = product.querySelector('.free-delivery--OD68c')?.innerText;
+        const image = product.querySelector('.image--Smuib')?.src;
+        const url = window.location.href;
+        const category = categoryType;
+
+        productList.push({
+          'prices': price,
+          'images': image,
+          'descriptions': description,
+          'title': title,
+          'url': url,
+          'category': category  
+        })
+
+      });
+
+      return productList;
+
+    } else {
+      
+      const priceElement = document.querySelector('.pdp-price.pdp-price_type_normal.pdp-price_color_orange.pdp-price_size_xl');
+      const titleElement = document.title;
+      const descriptionElement = document.querySelector('.html-content.pdp-product-highlights');
+      const imageElement = document.querySelector('.gallery-preview-panel__image');
+      const urlElement = window.location.href;
+      const categoryElement = document.querySelectorAll('.breadcrumb_item_anchor')[2];
+      return {
+        'price': priceElement ? priceElement.innerText : 'Price not found',
+        'imageElement': imageElement ? imageElement.src : 'Image not found',
+        'descriptionElement': descriptionElement ? descriptionElement.innerHTML : 'Description not found',
+        'titleElement': titleElement ? titleElement : 'Title not found',
+        'urlElement': urlElement ? urlElement : 'URL not found',
+        'categoryElement': categoryElement ? categoryElement.innerText : 'Category not found'
+      };
+    }
   }
 
 
